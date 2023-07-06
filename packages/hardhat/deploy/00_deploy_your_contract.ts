be1ec703718967de4995f83437b26aa7efa053b4
moltam89 <moltam89@gmail.com>
2023-07-06 09:46:40 +0200

    Deployed MyERC20 at 0x1a43e6741Ad9cE1FeAD3f9FEeb5C757f81C9131d

--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -31,7 +31,7 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
     autoMine: true,
   });
 */
-
+/*
   await deploy("RapidExample", {
     from: deployer,
     // Contract constructor arguments
@@ -41,6 +41,29 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
     // automatically mining the contract deployment transaction. There is no effect on live networks.
     autoMine: true,
   });
+*/
+
+  await deploy("MyERC20", {
+    from: deployer,
+    // Contract constructor arguments
+    args: ["MyToken", "MyToken", 18],
+    log: true,
+    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
+    // automatically mining the contract deployment transaction. There is no effect on live networks.
+    autoMine: true,
+  });
+/*
+  await deploy("RapidExample", {
+    from: deployer,
+    // Contract constructor arguments
+    //args: [deployer],
+    log: true,
+    // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
+    // automatically mining the contract deployment transaction. There is no effect on live networks.
+    autoMine: true,
+  });
+*/
+
 
   // Get the deployed contract
   // const yourContract = await hre.ethers.getContract("YourContract", deployer);
