2e1c89db735f5a0510c9ac36c1ff4ed1dfb6596e
moltam89 <moltam89@gmail.com>
2023-07-10 11:57:24 +0200

    GetManualOraclePrice (getEthPrice, getUSDCPrice) deployed at 0x00dCd65A8dc9322A499ccA4d83155C51ffBFEe42

--- packages/hardhat/contracts/GetManualOraclePrice.sol
+++ packages/hardhat/contracts/GetManualOraclePrice.sol
@@ -1,4 +1,6 @@
 interface IManualPayloadExample {
+    function getLatestEthPrice(bytes calldata redstonePayload) external view returns (uint256);
+    function getLatestUSDCPrice(bytes calldata redstonePayload) external view returns (uint256);
     function getLatestPrice(bytes calldata redstonePayload, bytes32 assetDataFeedId) external view returns (uint256);
 }
 
@@ -7,7 +9,15 @@ pragma solidity ^0.8.4;
 
 
 contract GetManualOraclePrice {
+  function getEthPrice(bytes calldata redstonePayload) public view returns (uint256) {
+    return IManualPayloadExample(0xCB01e966413706e87B1E7181a60A5e6ae88E877B).getLatestEthPrice(redstonePayload);
+  }
+
+  function getUSDCPrice(bytes calldata redstonePayload) public view returns (uint256) {
+    return IManualPayloadExample(0xCB01e966413706e87B1E7181a60A5e6ae88E877B).getLatestUSDCPrice(redstonePayload);
+  }
+
   function getPrice(bytes calldata redstonePayload, bytes32 assetDataFeedId) public view returns (uint256) {
-    return IManualPayloadExample(0x3B0AAD4e39F484e346125d64f936F12dB8fbBD04).getLatestPrice(redstonePayload, assetDataFeedId);
+    return IManualPayloadExample(0xCB01e966413706e87B1E7181a60A5e6ae88E877B).getLatestPrice(redstonePayload, assetDataFeedId);
   }
 }
