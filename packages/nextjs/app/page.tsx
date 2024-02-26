e9c5665ab28ee1cdddcdc32b1b1e76734c785d47
moltam89 <moltam89@gmail.com>
2024-02-25 20:23:55 -0700

    Number + Secret => blindedNumber

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -2,41 +2,59 @@
 
 import Link from "next/link";
 import type { NextPage } from "next";
-import { useAccount } from "wagmi";
 import { BugAntIcon, MagnifyingGlassIcon } from "@heroicons/react/24/outline";
-import { Address } from "~~/components/scaffold-eth";
+import { InputBase } from "~~/components/scaffold-eth";
+
+import { useEffect, useState } from "react";
+
+import { ethers } from "ethers";
 
 const Home: NextPage = () => {
-  const { address: connectedAddress } = useAccount();
+  const [number, setNumber] = useState<number | null>(null);
+  const [secret, setSecret] = useState<string | null>(null);
+
+  const [blindedNumber, setBlindedNumber] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (!number || !secret) {
+      return;
+    }
+
+    setBlindedNumber(ethers.utils.solidityKeccak256([ "uint256", "string" ], [ number, secret ]));
+
+  }, [number, secret]);
 
   return (
     <>
       <div className="flex items-center flex-col flex-grow pt-10">
         <div className="px-5">
           <h1 className="text-center">
-            <span className="block text-2xl mb-2">Welcome to</span>
-            <span className="block text-4xl font-bold">Scaffold-ETH 2</span>
+            <span className="block text-4xl font-bold">One Number to rule them all</span>
           </h1>
-          <div className="flex justify-center items-center space-x-2">
-            <p className="my-2 font-medium">Connected Address:</p>
-            <Address address={connectedAddress} />
-          </div>
-          <p className="text-center text-lg">
-            Get started by editing{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/nextjs/app/page.tsx
-            </code>
-          </p>
-          <p className="text-center text-lg">
-            Edit your smart contract{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              YourContract.sol
-            </code>{" "}
-            in{" "}
-            <code className="italic bg-base-300 text-base font-bold max-w-full break-words break-all inline-block">
-              packages/hardhat/contracts
-            </code>
-          </p>
+        </div>
+
+        <div>
+          <InputBase
+            onChange={
+              (value) => {
+                const number = parseInt(value);
+                if (!Number.isNaN(number) && (number > 0)) {
+                  setNumber(number);
+                }
+              }
+            }
+            placeholder={"Number"}
+          />
+          <InputBase
+            onChange={
+              (secret) => {
+                setSecret(secret);
+              }
+            }
+            placeholder={"Secret"}
+          />
+
+          {blindedNumber && blindedNumber}
         </div>
 
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
