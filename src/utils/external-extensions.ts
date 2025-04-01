df9672479a18ef6195f15e5c451bf7ac7719a21d
moltam89 <moltam89@gmail.com>
2025-04-01 11:37:49 -0400

    dunno whats wrong

--- src/utils/external-extensions.ts
+++ src/utils/external-extensions.ts
@@ -86,6 +86,8 @@ export const validateExternalExtension = async (
 
 // Gets the data from the argument passed to the `--extension` option.
 export const getDataFromExternalExtensionArgument = (externalExtension: string) => {
+  console.log("yoooo", externalExtension);
+
   if (CURATED_EXTENSIONS[externalExtension]) {
     externalExtension = getArgumentFromExternalExtensionOption(CURATED_EXTENSIONS[externalExtension]);
   }
