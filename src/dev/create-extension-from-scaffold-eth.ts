a11b391b770ddc0144e40a59d743e9cd69170249
moltam89 <moltam89@gmail.com>
2025-04-09 15:02:34 -0500

    Reorder

--- src/dev/create-extension-from-scaffold-eth.ts
+++ src/dev/create-extension-from-scaffold-eth.ts
@@ -13,58 +13,58 @@ export const DELETED_FILES_LOG = "deletedFiles.log";
 export const COMMIT_HASH_LOG = "commitHash.log";
 export const SOLIDITY_FRAMEWORK_LOG = "solidityFramework.log";
 
-const getDeletedAndRenamedFilesSinceCommit = async (projectName: string, commitHash: string): Promise<string[]> => {
-  const { stdout: gitOutput } = await execa(
-    "git",
-    ["diff", "--diff-filter=DR", "--name-status", `${commitHash}..HEAD`],
-    { cwd: projectName },
-  );
+export const createExtensionFromScaffoldEth = async (projectName: string, scaffoldEthRepo: string | null) => {
+  try {
+    if (scaffoldEthRepo) {
+      const { githubUrl, githubBranchUrl, branch } = parseExtensionString(scaffoldEthRepo);
 
-  // Process the output to extract deleted and renamed files
-  const deletedAndRenamedFiles = gitOutput
-    .split("\n") // Split into lines
-    .filter(Boolean) // Remove empty lines
-    .map(line => {
-      const parts = line.split("\t");
-      if (line.startsWith("D")) {
-        return parts[1]; // For deleted files, return the file name
-      } else if (line.startsWith("R")) {
-        return parts[1]; // For renamed files, return the original file name
-      }
-      return null; // Ignore other cases
-    })
-    .filter(Boolean); // Remove null entries
+      await assertRepoExists(githubBranchUrl, githubUrl);
 
-  return deletedAndRenamedFiles as string[];
-};
+      prettyLog.info(`Creating ${projectName} folder and cloning ${githubBranchUrl}...`, 1);
 
-const getChangedFilesSinceCommit = async (projectName: string, commitHash: string): Promise<string[]> => {
-  const { stdout } = await execa("git", ["diff", "--diff-filter=d", "--name-only", `${commitHash}..HEAD`], {
-    cwd: projectName,
-  });
+      await setUpRepository(githubUrl, projectName, branch);
 
-  return stdout.split("\n").filter(Boolean);
-};
+      prettyLog.success(`Cloned ${githubBranchUrl} into ${projectName}\n`, 1);
+    }
 
-const createDirectories = async (filePath: string, projectName: string) => {
-  const dirPath = path.join(EXTERNAL_EXTENSIONS_DIR, projectName, TARGET_EXTENSION_DIR, path.dirname(filePath));
-  await fs.promises.mkdir(dirPath, { recursive: true });
-};
+    prettyLog.info("Finding merge base commit hash...", 1);
+    const { mergeBaseCommitHash, solidityFramework } = await getMergeBaseCommitHash(projectName);
+    prettyLog.success(`Merge base commit hash: ${mergeBaseCommitHash}\n`, 1);
 
-const copyChangedFiles = async (changedFiles: string[], projectName: string) => {
-  for (const file of changedFiles) {
-    const sourcePath = path.resolve(projectName, file);
-    const destPath = path.join(EXTERNAL_EXTENSIONS_DIR, projectName, TARGET_EXTENSION_DIR, file);
-    if (!fs.existsSync(sourcePath)) continue;
-    await createDirectories(file, projectName);
-    await ncpPromise(sourcePath, destPath);
+    prettyLog.info("Getting list of changed files...", 1);
+    const changedFiles = await getChangedFilesSinceCommit(projectName, mergeBaseCommitHash);
+    const deletedAndRenamedFiles = await getDeletedAndRenamedFilesSinceCommit(projectName, mergeBaseCommitHash);
+
+    if (!changedFiles.length && !deletedAndRenamedFiles.length) {
+      prettyLog.warning("No files to process.");
+      return;
+    }
+
+    if (changedFiles.length) {
+      await copyChangedFiles(changedFiles, projectName);
+      prettyLog.success(`Copied ${changedFiles.length} changed files\n`, 1);
+    }
+
+    if (deletedAndRenamedFiles.length) {
+      await logData(projectName, DELETED_FILES_LOG, deletedAndRenamedFiles.join("\n"));
+    }
+
+    await logData(projectName, COMMIT_HASH_LOG, mergeBaseCommitHash);
+
+    await logData(projectName, SOLIDITY_FRAMEWORK_LOG, solidityFramework);
+
+    await initGitRepo(path.join(EXTERNAL_EXTENSIONS_DIR, projectName));
+
+    if (scaffoldEthRepo) {
+      const projectPath = path.join(process.cwd(), projectName);
+      await fs.promises.rm(projectPath, { recursive: true, force: true });
+      prettyLog.info(`Cleaned up temporary folder: ${projectPath}\n`, 1);
+    }
+
+    prettyLog.info(`Files processed successfully, updated ${EXTERNAL_EXTENSIONS_DIR}/${projectName} directory.`);
+  } catch (err: any) {
+    prettyLog.error(`Error: ${err.message}`);
   }
-};
-
-const logData = async (projectName: string, fileName: string, fileContent: string) => {
-  const logPath = path.join(EXTERNAL_EXTENSIONS_DIR, projectName, TARGET_EXTENSION_DIR, fileName);
-  await fs.promises.writeFile(logPath, fileContent, "utf8");
-  prettyLog.success(`${fileName} logged to ${logPath}\n`, 1);
 };
 
 const getMergeBaseCommitHash = async (
@@ -138,6 +138,62 @@ const getMergeBaseCommitHash = async (
   }
 };
 
+const getChangedFilesSinceCommit = async (projectName: string, commitHash: string): Promise<string[]> => {
+  const { stdout } = await execa("git", ["diff", "--diff-filter=d", "--name-only", `${commitHash}..HEAD`], {
+    cwd: projectName,
+  });
+
+  return stdout.split("\n").filter(Boolean);
+};
+
+const getDeletedAndRenamedFilesSinceCommit = async (projectName: string, commitHash: string): Promise<string[]> => {
+  const { stdout: gitOutput } = await execa(
+    "git",
+    ["diff", "--diff-filter=DR", "--name-status", `${commitHash}..HEAD`],
+    { cwd: projectName },
+  );
+
+  // Process the output to extract deleted and renamed files
+  const deletedAndRenamedFiles = gitOutput
+    .split("\n") // Split into lines
+    .filter(Boolean) // Remove empty lines
+    .map(line => {
+      const parts = line.split("\t");
+      if (line.startsWith("D")) {
+        return parts[1]; // For deleted files, return the file name
+      } else if (line.startsWith("R")) {
+        return parts[1]; // For renamed files, return the original file name
+      }
+      return null; // Ignore other cases
+    })
+    .filter(Boolean); // Remove null entries
+
+  return deletedAndRenamedFiles as string[];
+};
+
+const copyChangedFiles = async (changedFiles: string[], projectName: string) => {
+  for (const file of changedFiles) {
+    const sourcePath = path.resolve(projectName, file);
+    const destPath = path.join(EXTERNAL_EXTENSIONS_DIR, projectName, TARGET_EXTENSION_DIR, file);
+
+    if (!fs.existsSync(sourcePath)) continue;
+
+    await createDirectory(file, projectName);
+    await ncpPromise(sourcePath, destPath);
+  }
+};
+
+const createDirectory = async (filePath: string, projectName: string) => {
+  const dirPath = path.join(EXTERNAL_EXTENSIONS_DIR, projectName, TARGET_EXTENSION_DIR, path.dirname(filePath));
+  await fs.promises.mkdir(dirPath, { recursive: true });
+};
+
+const logData = async (projectName: string, fileName: string, fileContent: string) => {
+  const logPath = path.join(EXTERNAL_EXTENSIONS_DIR, projectName, TARGET_EXTENSION_DIR, fileName);
+  await fs.promises.writeFile(logPath, fileContent, "utf8");
+  prettyLog.success(`${fileName} logged to ${logPath}\n`, 1);
+};
+
 const initGitRepo = async (targetPath: string) => {
   try {
     const gitDir = path.join(targetPath, ".git");
@@ -152,59 +208,5 @@ const initGitRepo = async (targetPath: string) => {
     prettyLog.success(`Initialized git repository and made initial commit in ${targetPath}\n`, 1);
   } catch (error: any) {
     prettyLog.error(`Failed to initialize git repository: ${error.message}`);
-  }
-};
-
-export const createExtensionFromScaffoldEth = async (projectName: string, scaffoldEthRepo: string | null) => {
-  try {
-    if (scaffoldEthRepo) {
-      const { githubUrl, githubBranchUrl, branch } = parseExtensionString(scaffoldEthRepo);
-
-      await assertRepoExists(githubBranchUrl, githubUrl);
-
-      prettyLog.info(`Creating ${projectName} folder and cloning ${githubBranchUrl}...`, 1);
-
-      await setUpRepository(githubUrl, projectName, branch);
-
-      prettyLog.success(`Cloned ${githubBranchUrl} into ${projectName}\n`, 1);
-    }
-
-    prettyLog.info("Finding merge base commit hash...", 1);
-    const { mergeBaseCommitHash, solidityFramework } = await getMergeBaseCommitHash(projectName);
-    prettyLog.success(`Merge base commit hash: ${mergeBaseCommitHash}\n`, 1);
-
-    prettyLog.info("Getting list of changed files...", 1);
-    const changedFiles = await getChangedFilesSinceCommit(projectName, mergeBaseCommitHash);
-    const deletedAndRenamedFiles = await getDeletedAndRenamedFilesSinceCommit(projectName, mergeBaseCommitHash);
-
-    if (!changedFiles.length && !deletedAndRenamedFiles.length) {
-      prettyLog.warning("No files to process.");
-      return;
-    }
-
-    if (changedFiles.length) {
-      await copyChangedFiles(changedFiles, projectName);
-      prettyLog.success(`Copied ${changedFiles.length} changed files\n`, 1);
-    }
-
-    if (deletedAndRenamedFiles.length) {
-      await logData(projectName, DELETED_FILES_LOG, deletedAndRenamedFiles.join("\n"));
-    }
-
-    await logData(projectName, COMMIT_HASH_LOG, mergeBaseCommitHash);
-
-    await logData(projectName, SOLIDITY_FRAMEWORK_LOG, solidityFramework);
-
-    await initGitRepo(path.join(EXTERNAL_EXTENSIONS_DIR, projectName));
-
-    if (scaffoldEthRepo) {
-      const projectPath = path.join(process.cwd(), projectName);
-      await fs.promises.rm(projectPath, { recursive: true, force: true });
-      prettyLog.info(`Cleaned up temporary folder: ${projectPath}\n`, 1);
-    }
-
-    prettyLog.info(`Files processed successfully, updated ${EXTERNAL_EXTENSIONS_DIR}/${projectName} directory.`);
-  } catch (err: any) {
-    prettyLog.error(`Error: ${err.message}`);
   }
 };
